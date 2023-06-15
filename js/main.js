6879be5a48894442e007bb05e1d12150c8ab8cb3
alsomslo <133613694+alsomslo@users.noreply.github.com>
2023-06-16 01:02:23 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,13 +1,19 @@
 $(function () {
+    $(window).on('scroll', function () {
+        let sct = $(window).scrollTop();
+        sct > 0 ? $('.header').addClass('on') : $('.header').removeClass('on');
+    })
+
     $('.mainslide').slick({
         arrows: false,
         autoplay: 3000,
         dots: true,
 
     })
-    $('.pz_content li').on('click', function (e) {
+    $('.mainmenu li').on('click', function (e) {
         e.preventDefault();
         let idx = $(this).index();
+        $('.mainmenu li').eq(idx).toggleClass('on').siblings().removeClass('on');
         $('.pz_menu li').eq(idx).addClass('on').siblings().removeClass('on');
     })
 });
