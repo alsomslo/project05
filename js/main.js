f029928d62d2e2d5586b712bbd8230a976ab6a11
alsomslo <133613694+alsomslo@users.noreply.github.com>
2023-06-15 18:22:45 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -5,4 +5,9 @@ $(function () {
         dots: true,
 
     })
+    $('.pz_content li').on('click', function (e) {
+        e.preventDefault();
+        let idx = $(this).index();
+        $('.pz_menu li').eq(idx).addClass('on').siblings().removeClass('on');
+    })
 });
